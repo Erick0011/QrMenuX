f35aaf9a5813202da5722b77752a03342d85005e
eric <erick.baptista00@gmail.com>
2025-06-12 13:05:02 +0100

    m

---
+++ app/utils/generate_code.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from app.utils.now_angola import now_angola
+import secrets
+
+
+def generate_code():
+    now = now_angola().strftime("%y%m%d%H%M")  # Ex: "2506051230"
+    rand = secrets.token_hex(2)  # Ex: "a1b2"
+    return f"R-{now}-{rand}"  # Ex: "R-2506051230-a1b2"
